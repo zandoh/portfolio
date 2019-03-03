4c16dceb21af780f3ed4adec5e7bce07685b5565
Zachary Clark <zachary.clark@eagledream.com>
2019-03-03 17:48:20 -0500

    Design2: Switch to react-typed instead of typist

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Typist from "react-typist";
-import Detail from "../Detail/Detail";
+import Typed from "react-typed";
 import ContentfulClient from "../../contentful";
 import injectSheet from "react-jss";
 import styles from "./listStyles";
 
 class List extends Component {
-  typistList = null;
+  typistWords = [
+    "Front-End",
+    "Back-End",
+    "Internet of Things",
+    "Web Applications"
+  ];
   constructor(props) {
     super(props);
     this.state = {
       projects: []
     };
-    this.setTypistList();
   }
 
   async componentWillMount() {
@@ -23,29 +26,6 @@ class List extends Component {
     });
   }
 
-  setTypistList = () => {
-    const typistWords = [
-      "Front-End",
-      "Back-End",
-      "Internet of Things",
-      "Web Applications"
-    ];
-    this.typistList = typistWords.map((word, index) => {
-      return index < typistWords.length - 1 ? (
-        <span key={`typist-item-${index}`}>
-          {word}
-          <Typist.Backspace
-            count={word.length}
-            delay={500}
-            style={{ marginLeft: "1px" }}
-          />
-        </span>
-      ) : (
-        <span key={`typist-item-${index}`}>{word}</span>
-      );
-    });
-  };
-
   render() {
     const { classes } = this.props;
     return (
@@ -53,9 +33,16 @@ class List extends Component {
         <h2>Projects</h2>
         <div className={classes.typistContainer}>
           Developing projects pertaining to{" "}
-          <Typist className={`${classes.typist} dark`}>
-            {this.typistList}
-          </Typist>
+          <Typed
+            className={`${classes.typist} dark`}
+            strings={this.typistWords}
+            typeSpeed={40}
+            backSpeed={50}
+            smartBackspace
+            loop
+          >
+            <span />
+          </Typed>
         </div>
         {this.state.projects.map((project, index) => {
           return (
