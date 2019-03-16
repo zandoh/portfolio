5e2ec516fc93a24a49683020e85d5a990a053249
Zachary Clark <zachary.clark@eagledream.com>
2019-03-15 22:14:19 -0400

    Design2: Adds rainbow styling to log message

--- src/index.tsx
+++ src/index.tsx
@@ -41,12 +41,12 @@ ReactDOM.render(
 serviceWorker.register();
 
 const message = "Let's Chat!";
-const styles = [
-  "background: linear-gradient(#348F50, #56B4D3)",
-  "color: #FFF",
-  "font-size: 70px",
-  "line-height: 200px",
-  "padding-right: 50px"
+const rainbow = [
+  "font-weight: bold",
+  "font-size: 50px",
+  "color: red",
+  "margin-bottom: 15px",
+  "text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)"
 ].join(";");
 
-console.log(`%c ${message}`, styles);
+console.log(`%c ${message}`, rainbow);
