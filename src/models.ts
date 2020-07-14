919d4b989a4a275b520edfc5242c022522b65fba
Zachary Clark <zandoh>
2020-07-13 22:23:23 -0400

    Dev: rm -rf

--- src/models.ts
+++
@@ -1,6 +0,0 @@
-export interface IProject {
-  title: string;
-  imagePath: string;
-  imageAlt: string;
-  link: string;
-}
--- src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "Shared" {
-  export function AppLink<T>(props: { link: string; component: T }): void;
-}
-
-declare module "*.mp4" {
-  const src: string;
-  export default src;
-}
